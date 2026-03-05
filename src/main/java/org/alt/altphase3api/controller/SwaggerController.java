baf1b82ff5f882312f08f26f48b63ae09794b98e
Adrien Huhn <adrien.huhn@gmail.com>
2026-03-05 16:44:48 +0100

    feat: added SwaggerController to redirect from multiple paths

---
+++ src/main/java/org/alt/altphase3api/controller/SwaggerController.java
@@ -0,0 +1,12 @@
+package org.alt.altphase3api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SwaggerController {
+  @GetMapping({"/api/docs", "/docs", "/swagger-ui"})
+  public String redirectToSwagger() {
+    return "redirect:/swagger-ui.html";
+  }
+}
