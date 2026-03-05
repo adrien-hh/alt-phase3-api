2aece2a4f623bada76e1b890a12d9e4fcd859c3c
Adrien Huhn <adrien.huhn@gmail.com>
2026-03-05 13:45:26 +0100

    feat: added minimal ToolRepository to access the DB

---
+++ src/main/java/org/alt/altphase3api/repository/ToolRepository.java
@@ -0,0 +1,6 @@
+package org.alt.altphase3api.repository;
+
+import org.alt.altphase3api.domain.bo.Tool;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ToolRepository extends JpaRepository<Tool, Integer> {}
