04e66515627b49a0aa5ad9e5fd7ff2a76944dffe
Adrien Huhn <adrien.huhn@gmail.com>
2026-03-05 10:51:14 +0100

    chore: setting up project with Spring

---
+++ src/main/java/org/alt/altphase3api/AltPhase3ApiApplication.java
@@ -0,0 +1,13 @@
+package org.alt.altphase3api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltPhase3ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltPhase3ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/alt/altphase3api/AltPhase3ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alt.altphase3api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltPhase3ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
