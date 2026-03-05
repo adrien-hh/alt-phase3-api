69ff3b4c82a9c7e3dff8513c33ebcb8d6b30ef32
Adrien Huhn <adrien.huhn@gmail.com>
2026-03-05 17:18:51 +0100

    refactor: ToolController now uses input DTOs and returns HTTP reponses using ResponseEntity

--- src/main/java/org/alt/altphase3api/controller/ToolController.java
+++ src/main/java/org/alt/altphase3api/controller/ToolController.java
@@ -1,7 +1,12 @@
 package org.alt.altphase3api.controller;
 
+import jakarta.validation.Valid;
 import org.alt.altphase3api.domain.bo.Tool;
+import org.alt.altphase3api.dto.CreateToolRequest;
+import org.alt.altphase3api.dto.UpdateToolRequest;
 import org.alt.altphase3api.service.ToolService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,22 +22,24 @@ public class ToolController {
     }
 
     @GetMapping
-    public List<Tool> getTools() {
-        return toolService.getAllTools();
+    public ResponseEntity<List<Tool>> getTools() {
+        return ResponseEntity.ok(toolService.getAllTools());
     }
 
     @GetMapping("/{id}")
-    public Tool getTool(@PathVariable Integer id) {
-        return toolService.getToolById(id);
+    public ResponseEntity<Tool> getTool(@PathVariable Integer id) {
+        return ResponseEntity.ok(toolService.getToolById(id));
     }
 
     @PostMapping
-    public Tool createTool(@RequestBody Tool tool) {
-        return toolService.createTool(tool);
+    public ResponseEntity<Tool> createTool(@Valid @RequestBody CreateToolRequest request) {
+        Tool tool = toolService.createTool(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(tool);
     }
 
     @PutMapping("/{id}")
-    public Tool updateTool(@PathVariable Integer id, @RequestBody Tool tool) {
-        return toolService.updateTool(id, tool);
+    public ResponseEntity<Tool> updateTool(@PathVariable Integer id, @Valid @RequestBody UpdateToolRequest request) {
+        Tool tool = toolService.updateTool(id, request);
+        return ResponseEntity.ok(tool);
     }
 }
