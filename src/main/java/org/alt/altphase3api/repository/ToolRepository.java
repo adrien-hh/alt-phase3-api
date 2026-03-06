963e95b9e56b9ba2a62e6306aa1125e9f612f9ac
Adrien Huhn <adrien.huhn@gmail.com>
2026-03-06 13:16:59 +0100

    feat: adding JPA specifications for simpler filters implementation

--- src/main/java/org/alt/altphase3api/repository/ToolRepository.java
+++ src/main/java/org/alt/altphase3api/repository/ToolRepository.java
@@ -2,5 +2,6 @@ package org.alt.altphase3api.repository;
 
 import org.alt.altphase3api.domain.bo.Tool;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ToolRepository extends JpaRepository<Tool, Integer> {}
+public interface ToolRepository extends JpaRepository<Tool, Integer>, JpaSpecificationExecutor<Tool> {}
